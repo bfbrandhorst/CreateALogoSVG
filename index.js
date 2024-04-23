aa5b6b68b33ad1e9619b0c2e4bbc7da65abcfcc3
bfbrandhorst <bfbrandhorst@gmail.com>
2024-04-22 20:29:28 -0600

    prompt questions added in index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const inquirer = require('inquirer');
+
+
+inquirer.prompt(
+    [
+        {
+            type: 'input',
+            name: 'characters',
+            message: 'What text should go inside of the SVG (at most 3 characters)?',
+            validate: function (answer) {
+                if (answer.length > 3) {
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'What color should the text be (must be a valid color or a hexadecimal)?',
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'What shape should the logo be?',
+            choices: ['circle', 'triangle', 'square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'what color should the SVG be?'
+        }
+    ]
+)
